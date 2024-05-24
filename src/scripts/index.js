5cb1fd1d5e9d2d24d2eec1af23000f1f21be84a5
Alexis Guzman <spartanthelexvro@gmail.com>
2024-05-23 23:13:44 -0600

    Terminado

---
+++ src/scripts/index.js
@@ -0,0 +1,22 @@
+let btnUsuario = document.getElementById("usuario");
+let btnComprar = document.getElementById("comprar");
+let btnComprar1 = document.getElementById("comprar1");
+let btnComprar2 = document.getElementById("comprar2");
+let btnComprar3 = document.getElementById("comprar3");
+
+btnUsuario.addEventListener("click", () => {
+  window.location = "/public/login.html";
+});
+
+btnComprar.addEventListener("click", () => {
+  window.location = "/public/login.html";
+});
+btnComprar1.addEventListener("click", () => {
+  window.location = "/public/login.html";
+});
+btnComprar2.addEventListener("click", () => {
+  window.location = "/public/login.html";
+});
+btnComprar3.addEventListener("click", () => {
+  window.location = "/public/login.html";
+});
